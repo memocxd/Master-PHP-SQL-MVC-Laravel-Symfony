27cb22c86a3f663e4f27e66b9b46da01c09fe5ea
memocxd <yosoy@kevinzurita.com>
2020-08-31 23:44:17 -0500

    Ultimas practicas por hoy con while antes de dormir

---
+++ 09-bucles/index.php
@@ -0,0 +1,70 @@
+<?php 
+
+/* 
+//  BUCLE WHILE
+
+Estructura de control que itera o repite la ejecucion de una serie de instrucciones tantas veces como sea necesario, en base a una condicion
+
+while(condicion) {
+    bloque de instrucciones
+}
+
+*/
+
+$numero = 0;
+while ($numero <= 100) {
+    echo $numero;
+    if ($numero != 100) {
+        echo ', ';
+    }
+    $numero++;
+}
+
+echo '<hr>';
+
+// Ejemplo modificando la url de esta manera: 09-bucles/?numero=55
+if (isset($_GET['numero'])) {
+    $numero = (int)$_GET['numero'];
+}else {
+    $numero =1;
+}
+
+echo "<h1>Tabla de multiplicar del numero $numero</h1>";
+
+$contador = 1;
+while ($contador <= 10) {
+    echo "$numero x $contador = ". ($numero*$contador). '<br>';
+    $contador++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
