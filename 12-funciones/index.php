85406ec94acea30a36220e22469929aebb844d3a
memocxd <yosoy@kevinzurita.com>
2020-09-03 10:55:46 -0500

    Funciones, con return y funciones dentro de otras

--- 12-funciones/index.php
+++ 12-funciones/index.php
@@ -6,6 +6,8 @@ Conjunto de intrucciones agrupadas bajo un nombre concreto
 y que pueden reutilizarce invocando la funcion.
 */
 
+// Ejemplo 1
+
 function muestraNombres(){
     echo 'Kevin <br>';
     echo 'Memo <br>';
@@ -13,6 +15,8 @@ function muestraNombres(){
     echo 'Zurita <br>';
 }
 muestraNombres();
+
+// Ejemplo 2
 
 function tabla($numero) {
     echo "<h3> Tabla de multiplicar del numero $numero </h3>";
@@ -23,8 +27,92 @@ function tabla($numero) {
 }
 tabla(93);
 
-for ($i=1; $i <= 10 ; $i++) { 
+/* for ($i=1; $i <= 10 ; $i++) { 
     tabla($i);
+} */
+echo '<hr>';
+
+// Ejemplo 3
+
+function calculadora($numero1, $numero2, $negrita = false){
+    $suma = $numero1 + $numero2;
+    $resta = $numero1 - $numero2;
+    $multiplicacion = $numero1 * $numero2;
+    $division = $numero1 / $numero2;
+    $cadenaTexto = "";
+    
+    if ($negrita) {
+        $cadenaTexto .= '<h1>';
+    }
+    
+    $cadenaTexto .= "Suma: $suma <br>";
+    $cadenaTexto .= "Resta: $resta<br>";
+    $cadenaTexto .= "Multiplicacion: $multiplicacion<br>";
+    $cadenaTexto .= "Division: $division<br>";
+    
+    if ($negrita) {
+        $cadenaTexto .= '</h1>';
+    }
+    $cadenaTexto .= '<hr>';
+    return $cadenaTexto;
 }
+echo calculadora(10, 30, 1);
+print calculadora(15, 80);
+
+// Ejemplo 4 con return y funciones dentro de otras
+
+function getNombre($nombre){
+    $texto = "Los nombres son: $nombre";
+    return $texto;
+}
+
+function getApellidos($apellidos){
+    $texto = "Los apellidos son: $apellidos";
+    return $texto;
+}
+function devuelveElNombre($nombre, $apellidos){
+    $texto = getNombre($nombre)
+            .'<br>'.
+            getApellidos($apellidos);
+    return $texto;
+}
+echo devuelveElNombre("Guillermo Kevin", 'Mora Zurita');
+echo '<br>'. getNombre('Carlos');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
