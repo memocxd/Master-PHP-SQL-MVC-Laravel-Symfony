ae18dc9a337fd7e392fdb1cb6a19fa5f1c1ba64a
memocxd <yosoy@kevinzurita.com>
2020-09-08 13:09:56 -0500

    Crear, eliminar, leer, y ver contenido de una carpeta

--- 21-directorios/index.php
+++ 21-directorios/index.php
@@ -1,7 +1,24 @@
 <?php 
 
+// Para crear un directorio
+if (!is_dir('mi_carpeta')) {
+    mkdir('mi_carpeta', 0777) or die('No se puede crear la carpeta');
+    echo 'Carpeta creada exitosamente';
+}else {
+    echo 'Ya existe la carpeta <br>';
+    /* rmdir('mi_carpeta');
+    echo 'Carpeta eliminada'; */
+}
 
+echo '<hr>';
 
+if ($gestor = opendir('./mi_carpeta')) {
+    while (false !== ($archivo = readdir($gestor))) {
+        if ($archivo !='.' && $archivo != '..') {
+            echo $archivo. '<br>';
+        }
+    }
+}
 
 
 ?>
